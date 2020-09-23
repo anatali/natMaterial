776834bd2e24296617ec9687b06c7fd949f6a154
anatalibo <antonio.natali@unibo.it>
2020-09-23 12:12:02 +0200

    working with existing legacy

--- HealthAdapterFacade/DisiFhirConverter/constants/constants.js
+++
@@ -1,9 +0,0 @@
-// -------------------------------------------------------------------------------------------------
-// Copyright (c) Disi-Unibo. All rights reserved.
-// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// -------------------------------------------------------------------------------------------------
-
-var path = require('path');
-
-module.exports.TEMPLATE_FILES_LOCATION = path.join(__dirname, '../../DisiFhirConverter/templates');  
-module.exports.HL7_DATAFILES_LOCATION  = path.join(__dirname, '../../DisiFhirConverter/sampleData');  
