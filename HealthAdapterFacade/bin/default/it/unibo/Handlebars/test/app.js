1c102413a68792eb84731b871690178e468f107b
anatalibo <antonio.natali@unibo.it>
2020-10-01 11:10:23 +0200

    JPA

--- HealthAdapterFacade/bin/default/it/unibo/Handlebars/test/app.js
+++
@@ -1,8 +0,0 @@
-var context = {
-	"name":"Ritesh Kumar",
-	"occupation" : "Developer"
-}
-
-var templateScript = Handlebars.templates.demo(context);
-
-document.write(templateScript);
---
+++ accessing-data-rest/src/main/java/com/example/accessingdatarest/AccessingDataRestApplication.java
@@ -0,0 +1,28 @@
+package com.example.accessingdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataRestApplication.class, args);
+	}
+}
+
+/*
+curl http://localhost:8080/people	//DB VUOTO
+curl -i -H "Content-Type:application/json" -d "{\"firstName\": \"Frodo\", \"lastName\": \"Baggins\"}" http://localhost:8080/people
+curl http://localhost:8080/people
+curl http://localhost:8080/people/1
+curl http://localhost:8080/people/search		//CUSTOM QUERY
+curl http://localhost:8080/people/search/findByLastName?name=Baggins
+
+curl -X PUT -H "Content-Type:application/json" -d "{\"firstName\": \"Bilbo\", \"lastName\": \"Baggins\"}" http://localhost:8080/people/1
+curl -X PATCH -H "Content-Type:application/json" -d "{\"firstName\": \"Bilbo Jr.\"}" http://localhost:8080/people/1
+curl -X DELETE http://localhost:8080/people/1
+curl http://localhost:8080/people
+
+
+*/
