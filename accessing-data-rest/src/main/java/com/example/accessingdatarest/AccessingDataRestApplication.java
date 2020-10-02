89d7ea336974b107383f01b4286638c2f25d836c
anatalibo <antonio.natali@unibo.it>
2020-10-02 14:41:17 +0200

    mmodule

--- accessing-data-rest/src/main/java/com/example/accessingdatarest/AccessingDataRestApplication.java
+++ accessing-data-rest/src/main/java/com/example/accessingdatarest/AccessingDataRestApplication.java
@@ -24,5 +24,8 @@ curl -X PATCH -H "Content-Type:application/json" -d "{\"firstName\": \"Bilbo Jr.
 curl -X DELETE http://localhost:8080/people/1
 curl http://localhost:8080/people
 
+curl http://localhost:8080/accounts/send/41/42/500
 
-*/
+ * 
+ */
+
---
+++ springbootexamples/multimodule/mmlibrary/src/main/java/it/unibo/springboot/mmlibrary/service/MyService.java
@@ -0,0 +1,19 @@
+package it.unibo.springboot.mmlibrary.service;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableConfigurationProperties(ServiceProperties.class)
+public class MyService {
+
+  private final ServiceProperties serviceProperties;
+
+  public MyService(ServiceProperties serviceProperties) {
+    this.serviceProperties = serviceProperties;
+  }
+
+  public String message() {
+    return this.serviceProperties.getMessage();
+  }
+}
