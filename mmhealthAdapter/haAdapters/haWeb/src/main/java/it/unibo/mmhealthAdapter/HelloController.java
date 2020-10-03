8e298a895380c84182a63eebf2bfeedc94e9533c
Antonio Natali <antonio.natali@unibo.it>
2020-10-03 19:12:10 +0200

    mmhealthAdapter refactored

---
+++ mmhealthAdapter/haAdapters/haWeb/src/main/java/it/unibo/mmhealthAdapter/HelloController.java
@@ -0,0 +1,14 @@
+package it.unibo.mmhealthAdapter;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from HelloController in haWeb module";
+	}
+
+}
