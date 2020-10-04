f6222fb9c8f8e5ed7a1f8375fcd8bea123d1426d
Antonio Natali <antonio.natali@unibo.it>
2020-10-04 19:17:24 +0200

    mmhealthAdapter.html refactored

--- mmhealthAdapter/haConfiguration/src/main/java/it/unibo/mmhealthAdapterConfig/HealthAdapterApplication.java
+++ mmhealthAdapter/haConfiguration/src/main/java/it/unibo/mmhealthAdapterConfig/HealthAdapterApplication.java
@@ -12,22 +12,19 @@ import java.util.Arrays;
 public class HealthAdapterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(it.unibo.mmhealthAdapter.HealthAdapterApplication.class, args);
+		SpringApplication.run(HealthAdapterApplication.class, args);
 	}
-/*	
+/* 	
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
 				System.out.println(beanName);
 			}
-
 		};
 	}
-*/
+*/ 
 }
