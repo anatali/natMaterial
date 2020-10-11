453735a6ffff69aed1ee3ee9863edcb5db25b732
anatalibo <antonio.natali@unibo.it>
2020-10-11 17:40:45 +0200

    sandbox1

---
+++ smarteven-mock/src/main/java/com/itel/healthadapter/sandbox/smartevenmock/SmartEvenMockApplication.java
@@ -0,0 +1,11 @@
+package com.itel.healthadapter.sandbox.smartevenmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEvenMockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEvenMockApplication.class, args);
+    }
+}
---
+++ smarteven-mock/src/main/java/com/itel/healthadapter/sandbox/smartevenmock/SmartEvenMockConfiguration.java
@@ -0,0 +1,58 @@
+package com.itel.healthadapter.sandbox.smartevenmock;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IBasicClient;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.api.IRestfulClient;
+import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor;
+import com.itel.healthadapter.api.HealthAdapterAPI;
+import feign.Feign;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SmartEvenMockConfiguration {
+
+    private final SmartEvenMockConfigurationProperties configurationProperties;
+
+    public SmartEvenMockConfiguration(SmartEvenMockConfigurationProperties configurationProperties) {
+        this.configurationProperties = configurationProperties;
+    }
+
+    @Bean
+    public HealthAdapterAPI healthAdapterClient() {
+        Feign.Builder builder = Feign.builder();
+        builder.contract(new SpringMvcContract());
+//		builder.encoder(new JacksonEncoder());
+//		builder.decoder(new JacksonDecoder());
+        return builder.target(HealthAdapterAPI.class, configurationProperties.getHealthAdapterBaseUrl());
+    }
+
+    @Bean
+    public FhirContext fhirContext() {
+        return FhirContext.forR4();
+    }
+
+    @Bean
+    public ICustomFhirClient customFhirClient(FhirContext fhirContext) {
+        String serverBase = configurationProperties.getFhirServerBaseUrl();
+        ICustomFhirClient fhirClient = fhirContext.newRestfulClient(ICustomFhirClient.class, serverBase);
+        addBasicAuthInterceptor(fhirClient);
+        return fhirClient;
+    }
+
+    @Bean
+    public IGenericClient genericFhirClient(FhirContext fhirContext) {
+        FhirContext ctx = FhirContext.forR4();
+        String serverBase = configurationProperties.getFhirServerBaseUrl();
+        IGenericClient fhirClient = ctx.newRestfulGenericClient(serverBase);
+        addBasicAuthInterceptor(fhirClient);
+        return fhirClient;
+    }
+
+    private void addBasicAuthInterceptor(IRestfulClient client) {
+        BasicAuthInterceptor authInterceptor = new BasicAuthInterceptor(configurationProperties.getFhirServerUsername(), configurationProperties.getFhirServerPassword());
+        client.registerInterceptor(authInterceptor);
+    }
+}
