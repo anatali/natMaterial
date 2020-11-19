dc07da9126f6574bbfed67f66a3b61ae22ccb768
Antonio Natali <antonio.natali@unibo.it>
2020-11-19 18:45:29 +0100

    handlebars

---
+++ HealthAdapterFacade/src/main/java/it/unibo/Handlebars/templates/TestexNovo.java
@@ -0,0 +1,13 @@
+package it.unibo.Handlebars.templates;
+
+import it.unibo.Handlebars.UniboHandlebars;
+
+public class TestexNovo {
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("----------------------------------- exNovo" );
+        //UniboHandlebars.templateFile_parameterInMap();
+        UniboHandlebars.usingWith();
+    }
+}
